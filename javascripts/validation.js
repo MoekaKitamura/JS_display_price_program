1b6e10c3dbce658fdd274966d23038e3b86695eb
moeka kitamura <moeka.k76@gmail.com>
2022-09-14 01:25:04 +0900

    add real-time email validation

--- javascripts/validation.js
+++ javascripts/validation.js
@@ -1,10 +1,14 @@
 function emailValidation() {
   const form = document.forms[0]
+  
+  form.email_confirm.addEventListener("input", compareInputEmail);
+  form.email.addEventListener("input", compareInputEmail);
+
   form.addEventListener('submit', function(e) {
+    e.preventDefault();
     const name = form.name.value;
     if (confirm(`${name}さん本当に送信しますか？`)) {
       if (form.email.value !== form.email_confirm.value) {
-        e.preventDefault();
         const element = document.createElement("p")
         const message = document.createTextNode("Eメールが一致しません")
         form.appendChild(element);
@@ -13,11 +17,23 @@ function emailValidation() {
         setTimeout(function() {
           form.removeChild(element);
         }, 3000);
+      } else {
+        alert("送信したよ")
       }
     } else {
       alert("キャンセルしたよ")
     }
   });
+};
+
+function compareInputEmail() {
+  if (form.email.value !== form.email_confirm.value) {
+    document.getElementById("email_validation").style.display = "block"
+    form.email_confirm.style.background = "rgba(230,169,171,.5)"
+  } else {
+    document.getElementById("email_validation").style.display = "none"
+    form.email_confirm.style.background = "none"
+  }
 };
 
 window.onload = emailValidation;
